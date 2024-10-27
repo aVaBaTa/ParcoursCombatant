add917a9d3de99bff5328da30b9174e707e9d100
Simon <simong2004@hotmail.com>
2024-10-26 23:03:51 -0400

    Les Fonctions Capteur Prox
    les Fonctions sont dans le header file detecteur

--- src/detecteur.h
+++ src/detecteur.h
@@ -25,3 +25,140 @@ void Sifflet()
     start = 1;
   }
 }
+
+
+//Détecteurs de proximité
+int gaucheIRPin=A0;
+int droiteIRPin=A1;
+int valeurGaucheIR;
+int valeurDroiteIR;
+int seuilProximiteObjet = 200; //exemple pour environ 20 cm ** distance minimale du detecteur IR
+int DistanceObjet;
+String DirectionObjet;
+void AllerVersObjet(String Direction, int Distance){// peut rajouter confirmation
+
+  if(Direction == "Droite"){
+    /*Tourne a 90
+    Avance de la Distance*/
+    Serial.print("Tourne a Droite\n");
+    Serial.print("avance de ");
+    Serial.print(Distance);
+  }
+  else if(Direction == "Gauche"){
+
+    Serial.print("Tourne a Gauche");
+    Serial.print("avance de ");
+    Serial.print(Direction);
+  }
+  else if(Direction != "Droite" || Direction != "Gauche")
+  {
+    Serial.print("\n AllerVersObjet : error ==> Direction est mal ecrit ||   Droite ou Gauche");
+  }
+
+}
+ int Detecteur_IR_Distance()// a modifie la sortie pour etre en cm pour calculer la distance
+ {
+  int result;
+  valeurDroiteIR = analogRead(droiteIRPin);
+  valeurGaucheIR = analogRead(gaucheIRPin);
+
+  if(valeurDroiteIR >= seuilProximiteObjet and valeurGaucheIR >= seuilProximiteObjet)
+  {
+    Serial.print("Les deux detecteurs sont active");
+    result = -1;// a modifier
+  }
+  else if(valeurGaucheIR >= seuilProximiteObjet)
+  {
+    result = valeurGaucheIR;
+  }
+  else if(valeurDroiteIR >= seuilProximiteObjet)
+  {
+    result = valeurDroiteIR;
+  }
+  else if(seuilProximiteObjet > valeurDroiteIR and seuilProximiteObjet > valeurGaucheIR)
+  {
+    Serial.print("Aucun detecteur ne depasse le seuil");
+    result = -1;
+  }
+
+  return result;
+ }
+ int isObjectOutOfReach()
+ {
+  /*int pos
+  if(DistanceObjet >= pos){
+
+  }
+*/
+
+  return 0;
+ }
+float Detecteur_IR_Distance_TestMoyenne()// a modifie la sortie pour etre en cm pour calculer la distance
+ {
+  float result;
+  int TestValeurDroiteIR[10];
+  int TestValeurGaucheIR[10];
+
+  for(int i = 0; i < 10; i++){
+    TestValeurDroiteIR[i] = analogRead(droiteIRPin);
+    TestValeurGaucheIR[i] = analogRead(gaucheIRPin);
+  }
+  float moyenneDroiteIR = 0;
+  float moyenneGaucheIR = 0;
+  for(int i = 0; i < 10; i++)
+  {
+    moyenneDroiteIR += TestValeurDroiteIR[i];
+    moyenneGaucheIR += TestValeurGaucheIR[i];
+  }
+  moyenneDroiteIR = moyenneDroiteIR / 10;
+  moyenneGaucheIR = moyenneGaucheIR / 10;
+
+
+
+
+  if(moyenneDroiteIR >= seuilProximiteObjet and moyenneGaucheIR >= seuilProximiteObjet)
+  {
+    Serial.print("Les deux detecteurs sont active");
+    result = -1;// a modifier
+  }
+  else if(moyenneGaucheIR >= seuilProximiteObjet)
+  {
+    result = moyenneGaucheIR;
+  }
+  else if(moyenneDroiteIR >= seuilProximiteObjet)
+  {
+    result = moyenneDroiteIR;
+  }
+  else if(seuilProximiteObjet > moyenneDroiteIR and seuilProximiteObjet > moyenneGaucheIR)
+  {
+    Serial.print("Aucun detecteur ne depasse le seuil");
+    result = -1;
+  }
+
+  return result;
+ }
+int Detecteur_IR_Objet(){
+  int result;
+
+  if(Detecteur_IR_Distance() != -1){
+    DistanceObjet = Detecteur_IR_Distance();
+    if(valeurDroiteIR >= valeurGaucheIR){
+      DirectionObjet = "Droite";
+      result = 1;
+    }
+    else if(valeurDroiteIR < valeurGaucheIR){
+      DirectionObjet = "Gauche";
+      result = 2;
+    }
+    //if(DistanceObjet >= 0){} isObjectOutOfReach
+    //AllerVersObjet(DirectionObjet, DistanceObjet);   en dehors ou en dedans de la fonction
+  }
+  else if(Detecteur_IR_Distance() == -1)
+  {
+    Serial.print("No object detected");
+    result = -1;
+  }
+
+  return result;
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -15,292 +15,168 @@ void setup(){
   pinMode(rougepin, INPUT);
   delay(100);
   beep(3);
-
-
 }
-
-
-
-
-
 
 void loop()
 {
 
 
 
+
+
+
+
+  Detecteur_IR_Objet();
+
+  delay(500);
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*void loop() {
-  vert = digitalRead(vertpin);
-  rouge = digitalRead(rougepin);
-  if(etat == 0){Sifflet();}                                                                             // a ajouter
-  Serial.println(etat);
-  if (etat >= 1) // ou > 0
-  {
-    if (vert && y != 4) // Pas d'obstacle
-    {
-      if (x == 0)
-      {
-        if (y % 2 == 0)
-        {
-          if (regardeFace == 0)
-          {
-            setOrientation(1);
-          }
-          else if (regardeFace == -1)
-          {
-           
-            avance();
-            x -= 1;
-          }
-          else if (regardeFace == 1)
-          {
-           
-            avance();
-            x += 1;
-          }
-        }
-        else if (y % 2 != 0) // Vu qu'il est entre 2 lignes c'est obligé qu'il regarde en avant
-        {
-         
-          avance();
-          y += 1;
-        }
-      }
-      else if (x == 1)
-      {
-        if (regardeFace == 0)
-        {
-         
-          avance();
-          y += 1;
-        }
-        else if (regardeFace == 1)
-        {
-         
-          setOrientation(0);
-        }
-        else if (regardeFace == -1)
-        {
-         
-          avance();
-          x -= 1;
-        }
-      }
-        else if (x == -1)
-      {
-        if (regardeFace == 0)
-        {
-         
-          avance();
-          y += 1;
-        }
-        else if (regardeFace == -1)
-        {
-          Serial.println("setorientation");
-          setOrientation(0);
-        }
-        else if (regardeFace == 1)
-        {
-          Serial.println("avance");
-          avance();
-          x += 1;
-        }
-        last_move = 0;
-      }
-    }
+/*
+                                                          // CODE GEDEON
  
+//Pour le suiveur de igne
+int CaptLeft = A10;
+int CaptMid = A11;
+int CaptRight = A12;
+int ligneGauche;
+int ligneMilieu;
+int ligneDroite;
+
  
- 
- 
-    else if (vert && y == 4) // Pas d'obstacle et pas de mur au milieu.
-    {
-      if (x == 0)
-      {
-        if (y % 2 == 0)
-        {
-          if (regardeFace == 0)
-          {
-            avance();
-            y += 1;
-          }
-          else if (regardeFace == -1)
-          {
-           
-            avance();
-            x -= 1;
-          }
-          else if (regardeFace == 1)
-          {
-           
-            avance();
-            x += 1;
-          }
-        }
-        else if (y % 2 != 0) // Vu qu'il est entre 2 lignes c'est obligé qu'il regarde en avant
-        {
-         
-          avance();
-          y += 1;
-        }
-      }
-      else if (x == 1)
-      {
-        if (regardeFace == 0)
-        {
-         
-          avance();
-          y += 1;
-        }
-        else if (regardeFace == 1)
-        {
-         
-          setOrientation(0);
-        }
-        else if (regardeFace == -1)
-        {
-         
-          avance();
-          x -= 1;
-        }
-      }
-        else if (x == -1)
-      {
-        if (regardeFace == 0)
-        {
-         
-          avance();
-          y += 1;
-        }
-        else if (regardeFace == -1)
-        {
-          Serial.println("setorientation");
-          setOrientation(0);
-        }
-        else if (regardeFace == 1)
-        {
-          Serial.println("avance");
-          avance();
-          x += 1;
-        }
-        last_move = 0;
-      }
-    }
-    if (!vert) // Détection d'un obstacle
-    {
-      if (x == 0)
-      {
-        if (y % 2 == 0)
-        {
-          if (regardeFace == 0)
-          {
-            Serial.println("setOrientation");
-            setOrientation(1);
-            last_move = 1;
-          }
-          else if (regardeFace == 1 && last_move == 1)
-          {
-            Serial.println("setOrientation");
-            setOrientation(-1);
-            last_move = 0;
-          }
-          else if (regardeFace == 1 && last_move == 0)
-          {
-            Serial.println("setOrientation");
-            setOrientation(0);
-          }
-          else if (regardeFace == -1)
-          {
-            Serial.println("setOrientation");
-            setOrientation(0);
-          }
-        }
-      }
-      if (x == 1)
-      {
-        if(y % 2 == 0)
-        {
-          Serial.println("setOrientation");
-          setOrientation(-1);
-        }
-      }
-      if (x == -1)
-      {
-        if(y % 2 == 0)
-        {
-          Serial.println("setOrientation");
-          setOrientation(1);
-        }
-      }
-    }
-  }
-  if(y == 10)
-  {
-    if(x == 1)
-    {
-      tourneGauche();
-      avance();
-      tourneGauche();
-      x = 0;
-      avance_retour();
-      etat = 0;
-    }
-    else if(x == -1)
-    {
-      tourneDroit();
-      avance();
-      tourneDroit();
-      x = 0;
-      avance_retour();
-      etat = 0;
-    }
-  }
-  delay(100);
+// Gestion des cerveaux moteurs 
+//ajuster la tension VServo qui les alimentes à 7.2V max)
+void initialiserServoMoteur(){
+  SERVO_Enable(LEFT);
+  SERVO_Enable(RIGHT);
+  SERVO_SetAngle(LEFT,0);
+  SERVO_SetAngle(RIGHT,180);
 }
-*/
+ 
+void desactiverServoMoteur(){
+  SERVO_Disable(LEFT);
+  SERVO_Disable(RIGHT);
+}
+ 
+void ramasserObjet(){
+  SERVO_SetAngle(LEFT,90);
+  SERVO_SetAngle(RIGHT,90);
+}
+ 
+
+
+
+
+ 
+//Suiveur de ligne https://github.com/UdeS-GRO/LibRobus/wiki/Documentation-mat%C3%A9rielle#suiveur-de-ligne 
+
+
+
+ // delete si utiliser avancer***
+void avanceLent(){
+  resetEncodeurs();
+    MOTOR_SetSpeed(RIGHT,vitesse);
+    MOTOR_SetSpeed(LEFT, vitesse);
+    pulses_droit = ENCODER_Read(RIGHT);
+};
+ 
+
+
+void ajusterdroite(){
+ 
+  ligneGauche = analogRead(CaptLeft);
+  ligneMilieu = analogRead(CaptMid);
+  ligneDroite = analogRead(CaptRight);
+ 
+  while ((ligneGauche > 40) && (ligneMilieu < 640))
+  {
+    resetEncodeurs();
+    while (pulses_gauche < 600) // 1800 pulses = 950ms représente le nombre de pulses avant que la fonction tourneDroit arrête
+    {
+      MOTOR_SetSpeed(RIGHT, 0.5*vitesse);
+      MOTOR_SetSpeed(LEFT, vitesse);
+      pulses_gauche = ENCODER_Read(LEFT);
+    }
+  }
+}
+ 
+void ajustergauche(){
+ 
+  ligneGauche = analogRead(CaptLeft);
+  ligneMilieu = analogRead(CaptMid);
+  ligneDroite = analogRead(CaptRight);
+ 
+  while ((ligneDroite > 40) && (ligneMilieu < 640) && (pulses_droit < 600))
+  {
+    resetEncodeurs();
+    while (pulses_droit < 600) // 1800 pulses = 950ms représente le nombre de pulses avant que la fonction tourneDroit arrête
+    {
+      MOTOR_SetSpeed(RIGHT, vitesse);
+      MOTOR_SetSpeed(LEFT, 0.5*vitesse);
+      pulses_droit = ENCODER_Read(RIGHT);
+    }
+  }
+}
+ 
+void suivreLigne(){
+ 
+  ligneGauche = analogRead(CaptLeft);
+  ligneMilieu = analogRead(CaptMid);
+  ligneDroite = analogRead(CaptRight);
+  Serial.println(ligneDroite);
+  Serial.println(ligneGauche);
+  Serial.println(ligneMilieu);
+ 
+  if ((ligneMilieu > 640) && (ligneDroite < 40) && (ligneGauche < 40)){
+    avanceLent();
+  }
+ 
+  if ((ligneDroite > 650) &&  (ligneMilieu < 40) && (ligneGauche < 40)){
+    ajusterdroite();
+  }
+ 
+  if ((ligneGauche > 610) && (ligneMilieu < 40) && (ligneDroite < 40)){
+    ajustergauche();
+  }
+ 
+  if ((ligneMilieu < 40) && (ligneDroite < 40) && (ligneGauche < 40)){
+    arret();
+  }
+ 
+  if ((ligneMilieu > 640) && (ligneDroite > 650) && (ligneGauche > 610)){
+    arret();
+  }
+ 
+  if ((ligneMilieu > 640) && (ligneDroite > 650) && (ligneGauche < 40)){
+    ajusterdroite();
+  }
+ 
+  if ((ligneMilieu > 640) && (ligneDroite < 40) && (ligneGauche > 610)){
+    ajustergauche();
+  }
+  /*
+    Lorsqu'on est sur le planche blanc
+    -------------------------------
+            |       Gauche        |
+    Vide    |      24 - 30          
+    Ligne   |  319 - 458 - 1023
+    -------------------------------
+ 
+    -------------------------------
+            |       Milieu        |
+    Vide    |      28 - 37          
+    Ligne   |  649 - 759 - 1023
+    -------------------------------
+ 
+    -------------------------------
+            |       Droit         |
+    Vide    |      25 - 33          
+    Ligne   |  558 - 655 - 1023
+    -------------------------------
+  */
+//}
+ 
+ 
+ 
